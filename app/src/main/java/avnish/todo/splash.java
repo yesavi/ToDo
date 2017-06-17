71519a39e10ad0933b11701229adee0de3b49965
Avnish Pandey <avnishpandey8@gmail.com>
2017-06-17 12:08:27 +0430

    first commit

---
+++ app/src/main/java/avnish/todo/splash.java
@@ -0,0 +1,39 @@
+package avnish.todo;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Avnish on 6/17/2017.
+ */
+
+public class splash extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        // TODO Auto-generated method stub
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash);
+
+        Thread timerThread = new Thread() {
+            public void run() {
+                try {
+                    sleep(3000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                } finally {
+                    Intent intent = new Intent(splash.this, MainActivity.class);
+                    startActivity(intent);
+                }
+            }
+        };
+        timerThread.start();
+    }
+
+    @Override
+    protected void onPause() {
+        // TODO Auto-generated method stub
+        super.onPause();
+        finish();
+    }
+}
